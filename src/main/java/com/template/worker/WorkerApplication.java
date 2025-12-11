45e9e9e5d3b2bb226869147c6fa241c8106bafd4
swthewhite <swthewhite@gmail.com>
2025-12-11 13:38:30 +0900

    style: apply spotless

--- src/main/java/com/template/worker/WorkerApplication.java
+++ src/main/java/com/template/worker/WorkerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkerApplication {
 
     public static void main(String[] args) {
-        System.exit(
-                SpringApplication.exit(SpringApplication.run(WorkerApplication.class, args)));
+        System.exit(SpringApplication.exit(SpringApplication.run(WorkerApplication.class, args)));
     }
 }
