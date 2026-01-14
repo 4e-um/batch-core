4c626a8d15e99040215c1dca0885003bfe2b9e71
swthewhite <swthewhite@gmail.com>
2026-01-14 09:39:44 +0900

    test: add unit tests for BatchAdminController and BatchAdminService

--- src/test/java/com/template/worker/WorkerApplicationTests.java
+++ src/test/java/com/template/worker/WorkerApplicationTests.java
@@ -8,6 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class WorkerApplicationTests {
 
-    @Test
-    void contextLoads() {}
+  @Test
+  void contextLoads() {}
 }
