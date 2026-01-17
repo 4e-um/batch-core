336f8960fbc9aa192382253e3a1493f171e558d5
swthewhite <swthewhite@gmail.com>
2026-01-17 17:06:19 +0900

    UPLUS-90 fix: 테스트 시 BatchJobRunner NPE 방지를 위해 MockitoBean 처리
    - SpringBootTest 컨텍스트 로드 시 ApplicationRunner가 실행되면서
      job 파라미터가 없어 NPE 발생.
    - Runner는 부트스트랩 코드이므로
      테스트에서 모킹하여 제외.

--- src/test/java/com/template/worker/WorkerApplicationTests.java
+++ src/test/java/com/template/worker/WorkerApplicationTests.java
@@ -1,12 +1,16 @@
 package com.template.worker;
 
+import com.template.worker.global.runner.BatchJobRunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class WorkerApplicationTests {
+
+  @MockitoBean private BatchJobRunner batchJobRunner;
 
   @Test
   void contextLoads() {}
