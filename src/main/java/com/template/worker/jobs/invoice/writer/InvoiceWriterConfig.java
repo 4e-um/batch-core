d4e469706ba330dd0a06870b4ef49578055c2fef
yechan <100present@naver.com>
2026-01-20 16:18:11 +0900

    UPLUS-93 feat: timezone 수정

--- src/main/java/com/template/worker/jobs/invoice/writer/InvoiceWriterConfig.java
+++ src/main/java/com/template/worker/jobs/invoice/writer/InvoiceWriterConfig.java
@@ -59,7 +59,7 @@ public class InvoiceWriterConfig {
                                 to_date(:invMonth, 'YYYYMM') - interval '1 second',
                                 -- 납기일: 당월 말일 23:59:59
                                 to_date(:invMonth, 'YYYYMM') + interval '1 month' - interval '1 second',
-                                now() AT TIME ZONE 'Asia/Seoul'
+                                now()
                             FROM subscription s
                             JOIN customer c
                             ON s.customer_id = c.customer_id
