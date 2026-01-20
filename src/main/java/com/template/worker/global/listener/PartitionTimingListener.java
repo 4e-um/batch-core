76d0387dd255eeab5e450e11d17a9e64f8e27075
yechan <100present@naver.com>
2026-01-20 14:41:24 +0900

    UPLUS-93 feat: PartitionListenr 통합

---
+++ src/main/java/com/template/worker/global/listener/PartitionTimingListener.java
@@ -0,0 +1,33 @@
+package com.template.worker.jobs.invoice.step;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PartitionTimingListener implements StepExecutionListener {
+
+    private long start;
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        start = System.currentTimeMillis();
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        long duration = System.currentTimeMillis() - start;
+
+        log.info(
+                "[PARTITION] name={} read={} write={} time={}ms",
+                stepExecution.getStepName(),
+                stepExecution.getReadCount(),
+                stepExecution.getWriteCount(),
+                duration
+        );
+        return stepExecution.getExitStatus();
+    }
+}
