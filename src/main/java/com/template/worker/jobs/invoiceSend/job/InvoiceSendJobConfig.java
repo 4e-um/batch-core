abf45e4a9a3c8ba75ff7bc5368f78c333d84e1da
andrew <leegd120@gmail.com>
2026-01-25 21:33:26 +0900

    UPLUS-141 feat: 배치 JobConfig 작성

---
+++ src/main/java/com/template/worker/jobs/invoiceSend/job/InvoiceSendJobConfig.java
@@ -0,0 +1,55 @@
+package com.template.worker.jobs.invoiceSend.job;
+
+import com.template.worker.jobs.invoiceSend.model.InvoiceAggregateRecord;
+import com.template.worker.jobs.invoiceSend.model.InvoiceSendRecord;
+import com.template.worker.jobs.invoiceSend.processor.InvoiceSendProcessor;
+import com.template.worker.jobs.invoiceSend.writer.InvoiceSendWriter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class InvoiceSendJobConfig {
+
+    private static final int CHUNK_SIZE = 10000;
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    private final InvoiceSendProcessor invoiceSendProcessor;
+    private final InvoiceSendWriter invoiceSendWriter;
+
+    @Bean
+    public Job invoiceSendJob(Step invoiceSendStep) {
+        return new JobBuilder("invoiceSendJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(invoiceSendStep)
+                .build();
+    }
+
+    @Bean
+    public Step invoiceSendStep(
+            JdbcCursorItemReader<InvoiceSendRecord> invoiceJoinReader
+    ) {
+        return new StepBuilder("invoiceSendStep", jobRepository)
+                .<InvoiceSendRecord, InvoiceAggregateRecord>chunk(
+                        CHUNK_SIZE,
+                        transactionManager
+                )
+                .reader(invoiceJoinReader)
+                .processor(invoiceSendProcessor)
+                .writer(invoiceSendWriter)
+                .listener(invoiceSendProcessor)
+                .listener(invoiceSendWriter)
+                .build();
+    }
+}
